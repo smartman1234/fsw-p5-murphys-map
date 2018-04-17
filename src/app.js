0319da7d31321f3fa78f020605516859c18da984
smartman1234 <john.smith.dev0724@gmail.com>
2018-04-17 12:06:27 -0300

    feat: Add hamburger slider menu for small screens

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // App Entry Point ----------------------------------------
 
 import * as Map from './map';
+import * as $ from 'jquery';
 
 // Import css/html for hot reloading
 // TODO - Does this need to be done in js? Should it be constrained to dev only?
@@ -10,3 +11,22 @@ import '../index.html'
 // Set our GoogleMaps API callback functions
 window.initMap = Map.initMap;
 window.mapLoadingError = Map.mapLoadingError;
+
+$(document).ready(function () {
+	// Implement hamburger menu toggle on small screens
+	$('.hamburger-open').click(function () {
+		$('.filter-by').slideToggle("slow");
+		$('.place-list').slideToggle("slow", function () {
+			$('.hamburger-open').hide();
+			$('.hamburger-close').show();
+		});
+	});
+
+	$('.hamburger-close').click(function () {
+		$('.filter-by').slideToggle("slow");
+		$('.place-list').slideToggle("slow", function () {
+			$('.hamburger-close').hide();
+			$('.hamburger-open').show();
+		});
+	});
+});
